02b631114134d5820a653d73ccee05a7a44b58a7
johncduran <john.duran@stonybrook.edu>
2023-08-08 23:31:34 +0000

    put in module exmaple

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-import module1
+import module1 as mod1
 
-output1 = module1.addition(5,10)
-output2 = module1.subtraction(5,10)
+output1 = mod1.addition(5,10)
+output2 = mod1.subtraction(5,10)
+
+print('Output 1:', output1)
+print('Output 2:', output2)
+
+print(f"Output 1:{output1}")
+print(f'Output 2:{output2}')
+
+#imported here just these two functions since they were the only ones that we needed.
+
+
+from module1 import addition, subtraction
+
+output1 = addition(5,10)
+output2 = subtraction(5,10)
 
 print('Output 1:', output1)
 print('Output 2:', output2)
